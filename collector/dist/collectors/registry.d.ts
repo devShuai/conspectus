c83eb0ce188043655304d610d21bc72c5ce672d5
devShuai <shshilmh@gmail.com>
2026-08-08 01:47:19 +0800

    chore(collector): ignore node_modules and dist

--- collector/dist/collectors/registry.d.ts
+++
@@ -1,3 +0,0 @@
-import type { LocalCollector } from "../types.js";
-export declare function registerCollector(collector: LocalCollector): void;
-export declare function listCollectors(): LocalCollector[];
--- collector/dist/collectors/registry.js
+++
@@ -1,7 +0,0 @@
-const registry = [];
-export function registerCollector(collector) {
-    registry.push(collector);
-}
-export function listCollectors() {
-    return [...registry];
-}
